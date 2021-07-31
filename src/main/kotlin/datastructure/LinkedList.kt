6f4e3c18deb639f4475f191335eff791627d9dbf
Mehdi <mehdimjt@gmail.com>
2021-07-31 12:19:22 +0430

    Update LinkedList

--- src/main/kotlin/datastructure/LinkedList.kt
+++ src/main/kotlin/datastructure/LinkedList.kt
@@ -1,5 +1,10 @@
 package datastructure
 
+/**
+ * A node is like a single element of a chain that responsible to:
+ * hold a value
+ * hold a reference to the next node(next element of chain) until null which means the end of the list
+ */
 class Node<T>(var value: T, var next: Node<T>? =  null) {
     override fun toString(): String {
         return if (next != null) {
